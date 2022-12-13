36300987fd3e7b028028de0e1b45dcffdd6c7609
mobek12 <laoti.movek@gmail.com>
2022-12-13 17:47:05 +0545

    day 1 of js

---
+++ day5.js
@@ -0,0 +1,27 @@
+// var a=1;
+// var b=1
+
+// const c=3;
+// cannot reassign value
+// let firstname = "yuri"
+// console.log(firstname)
+// let a="12";
+// let b=12;
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a-b)
+
+// 
+// let loggedin=true;
+// let issuperuser=true;
+// let t = isloggedin &&issuperuser;
+let arr = [1,2,3,4,5,6,7]
+
+let objects={name:'mobek',age:'27'}
+console.log(objects.age)
+console.log(objects.name)
+console.log(objects["name"])
+console.log(objects[name])
+
+let object1={Fruit1:'Mango',Fruit2:'Apple'}
+console.log(object1.Fruit1)
